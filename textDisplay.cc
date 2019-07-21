15ae83fc089b7368da3909e0ad3d3cd38c73c17d
Junjie He <j247he@uwaterloo.ca>
2019-07-21 15:11:43 -0400

    TextDisplay implement

---
+++ textDisplay.cc
@@ -0,0 +1,39 @@
+#include <fstream>
+#include <sstream>
+#include "textDisplay.h"
+
+using namespace std;
+
+TextDisplay::TextDisplay(string map = "map.txt"){
+    ifstream m(map);
+    char c;
+    vector<char> row;
+    while (m.get(c)){
+        if(c == '\n'){
+            theDisplay.emplace_back(row);
+            row.clear();
+        } else {
+            row.emplace_back(c);
+        }
+    }
+}
+
+void TextDisplay::notify(int x, int y, char type){
+    theDisplay[x][y] = type;
+}
+
+ostream &operator<<(ostream & out, const TextDisplay & td){
+    for (auto i : td.theDisplay){
+        for (auto j : i){
+            cout << j;
+        }
+        cout << endl;
+    }
+}
+
+
+
+
+
+
+
--- textDisplay.h
+++ textDisplay.h
@@ -1,6 +1,18 @@
 #ifndef TEXTDISPLAY_H
 #define TEXTDISPLAY_H
+#include <vector>
+#include <iostream>
+#include <string>
+#include "floor.h"
+
+using std::vector;
 
 class TextDisplay {
+    vector<vector<char>> theDisplay;
+    public:
+    TextDisplay::TextDisplay(std::string map = "map.txt");
+    void notify(int x, int y, char type);
+    friend std::ostream &operator<<(std::ostream &out, const Floor &);
+    friend std::ostream &operator<<(std::ostream &out, const TextDisplay &);
 };
 #endif
