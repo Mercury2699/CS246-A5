d26005e6dddf2a8b12fe8930d6ece169bfb15a42
陈泉澄 <chenquancheng@v1040-wn-rt-c-9-101.campus-dynamic.uwaterloo.ca>
2019-07-23 22:01:40 -0400

    potion

--- potion.h
+++ potion.h
@@ -14,23 +14,59 @@ class Potion : public Item {
 
 class BoostDef : public Potion {
 	public:
-	void effect(Player &pc) override {
-		pc.setDef(pc.getDef() + 5);
+	void effect(Elves &e) override {
+		e.setDef(e.getDef() + 5);
+	}
+	void effect(Human &h) override {
+		h.setDef(h.getDef() + 5);
+	}
+
+	void effect(Dwarf &d) override {
+		d.setDef(d.getDef() + 5);
+	}
+
+	void effect(Orc &o) override {
+		o.setDef(o.getDef() + 5);
 	}
 };
 
 class BoostAtk : public Potion {
 	public:
-	void effect(Player &pc) override {
-		pc.setAtk(pc.getAtk() + 5);
+	void effect(Elves &e) override {
+		e.setAtk(e.getAtk() + 5);
+	}
+	void effect(Human &h) override {
+		h.setAtk(h.getAtk() + 5);
+	}
+
+	void effect(Dwarf &d) override {
+		d.setAtk(d.getAtk() + 5);
+	}
+
+	void effect(Orc &o) override {
+		o.setAtk(o.getAtk() + 5);
 	}
 };
 
 class RestorHP : public Potion {
 	public:
-	void effect(Player &pc) override {
-		int newHP = (pc.getHP() + 10) <= pc.getMaxHP() ? (pc.getHP() + 10) : pc.getMaxHP();
-		pc.setHP(newHP);
+	void effect(Elves &e) override {
+		int newHP = (e.getHP() + 10) <= e.getMaxHP() ? (e.getHP() + 10) : e.getMaxHP();
+		e.setHP(newHP);
+	}
+	void effect(Human &h) override {
+		int newHP = (h.getHP() + 10) <= h.getMaxHP() ? (h.getHP() + 10) : h.getMaxHP();
+		h.setHP(newHP);
+	}
+
+	void effect(Dwarf &d) override {
+		int newHP = (d.getHP() + 10) <= d.getMaxHP() ? (d.getHP() + 10) : d.getMaxHP();
+		d.setHP(newHP);
+	}
+
+	void effect(Orc &o) override {
+		int newHP = (o.getHP() + 10) <= o.getMaxHP() ? (o.getHP() + 10) : o.getMaxHP();
+		o.setHP(newHP);
 	}
 };
 
